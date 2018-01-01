bab6d9be8f186eef1ab02573b1cb56fed061d749
OstapL <ostap-v-30@mail.ru>
2018-01-01 17:55:15 +0200

    added folder store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import './foundation';
+import * as firebase from 'firebase'
 
 import App from './App';
 import router from './router';
-
+import { store } from './store'
 Vue.use(VueResource);
 
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
+  created() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyA_7ylkMKqy855O7iiJ3WHSfMILl1pv2sQ",
+        authDomain: "webdev-vue.firebaseapp.com",
+        databaseURL: "https://webdev-vue.firebaseio.com",
+        projectId: "webdev-vue",
+        storageBucket: "webdev-vue.appspot.com",
+        messagingSenderId: "88701941783",
+    })
+  }
 });
